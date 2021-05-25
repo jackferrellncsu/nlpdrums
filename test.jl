151fb5698a8c95566a7879db2d04578ea1ecfd65
mlovig <mlovig@ncsu.edu>
2021-05-25 14:59:41 -0400

    Update test.jl

--- test.jl
+++ test.jl
@@ -1,4 +1,20 @@
+#=
 using CSV
 using DataFrames
 arr = CSV.read("/Users/mlovig/Downloads/mtsamples.csv", DataFrame)
-print(arr[1,:])
+=#
+
+for i in 1:4999
+    if ismissing(arr[i,5]) == false
+        println(i)
+        arr[i,5] = replace(arr[i,5], ".," => " ")
+        arr[i,5] = replace(arr[i,5], [',',';','.',')','('] => "")
+        arr[i,5] = replace(arr[i,5], r":" => ": ")
+        arr[i,5] = replace(arr[i,5], r"\s\s+"=> " ")
+        arr[i,5] = lowercase(arr[i,5])
+    end
+end
+field = arr[:, 3]
+trans = arr[:, 5]
+println(field[1])
+println(trans[1])
