b08f19966c4986575c70717054d86d804d458379
mlovig <mlovig@ncsu.edu>
2021-05-26 08:45:41 -0400

    Update test.jl
    Included data cleaning

--- test.jl
+++ test.jl
@@ -1,12 +1,12 @@
-#=
+
 using CSV
 using DataFrames
 arr = CSV.read("/Users/mlovig/Downloads/mtsamples.csv", DataFrame)
-=#
+
 
 for i in 1:4999
     if ismissing(arr[i,5]) == false
-        println(i)
+        #println(i)
         arr[i,5] = replace(arr[i,5], ".," => " ")
         arr[i,5] = replace(arr[i,5], [',',';','.',')','('] => "")
         arr[i,5] = replace(arr[i,5], r":" => ": ")
@@ -14,7 +14,28 @@ for i in 1:4999
         arr[i,5] = lowercase(arr[i,5])
     end
 end
+
 field = arr[:, 3]
 trans = arr[:, 5]
-println(field[1])
-println(trans[1])
+
+for i in reverse(1:4999)
+    if ismissing(trans[i]) == true
+        global field = field[1:end.!=i]
+        global trans = trans[1:end.!=i]
+    end
+end
+
+uniField = [[],[]]
+
+
+for n in 1:4966
+    if field[n] ∉ uniField[[1]]
+        print(field[n,:])
+        push!(uniField[1], field[n,:])
+        push!(uniField[2], 1)
+    else
+        for ii in uniField[1]
+            uniField[2,findall(x->x=ii)]+= 1
+        end
+    end
+end
