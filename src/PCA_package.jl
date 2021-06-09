83c395a4399d00129d3fac9f791da37598342dfc
jackferrell <jferrel3@ncsu.edu>
2021-06-09 09:55:53 -0400

    figure out why logit broke

--- src/PCA_package.jl
+++ src/PCA_package.jl
@@ -16,7 +16,7 @@ using Lathe.preprocess: TrainTestSplit
 data = importClean()
 dtmi = CreateDTM(data, " Cardiovascular / Pulmonary")
 df = DataFrame(1.0*dtmi', :auto)
-train, test = TrainTestSplit(df, .9);
+train, test = TrainTestSplit(df, .9)
 
 errors = []
 for i in 1:100
@@ -32,6 +32,32 @@ end
 
 plot(1:100,errors)
 
+errors2 = []
+for B in 1:100
+    for bb in 1:1000
+        train, test = TrainTestSplit(df, .9)
+
+        MultivariateStats.fit(MultivariateStats.PCA, Matrix(train)[:, 1:end-1], B)
+
+        model = glm()
+    end
+end
+df_2 = DataFrame(1.0*dtmi, :auto)
+train, test = TrainTestSplit(df_2, .9)
+
+trainPreds = train[1:end - 1, :]
+trainOutcomes = convert(Array{Any}, train[end, :])
+
+M = MultivariateStats.fit(MultivariateStats.PCA, Matrix(trainPreds); maxoutdim = 100)
+pcaX = MultivariateStats.transform(M, Matrix(trainPreds))
+
+pca_trainDf = DataFrame(hcat(pcaX', trainOutcomes))
+
+model = glm(term(Symbol(:x, 101)) ~ sum(term.(Symbol.(names(pca_trainDf[:, Not(Symbol(:x, 101))])))),
+                                            pca_trainDf, Binomial(), ProbitLink())
+
+model = glm(term(Symbol(:x, 101)) ~ sum(term.(Symbol.(names(pca_trainDf, Not(:x101))))),
+                                            pca_trainDf, Binomial(), LogitLink())
 #=
 rocnums = MLBase.roc(test[:,end] .== 1,vec(rets), 50)
 
