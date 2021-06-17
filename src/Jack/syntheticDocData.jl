ed0b196e5b18a8811a313127e3ab5ec66bd1af13
jackferrell <jferrel3@ncsu.edu>
2021-06-17 15:14:30 -0400

    :)

--- src/Jack/syntheticDocData.jl
+++ src/Jack/syntheticDocData.jl
@@ -1,66 +1,129 @@
 using CSV
 using Random
+using Word2Vec
 
 Random.seed!(1234)
 
+#Setting constant parameters for data creation
+struct SynthParams
+    NUMDOCS
+    MIN_DOC_LENGTH
+    MAX_DOC_LENGTH
+
+    P_HOT_DOC
+    P_KEY_PHRASE_1
+    P_KEY_PHRASE_0
+    DIST1
+    DIST2
+end
+
 words = ['A':'Z';]
 
-#Setting constant parameters for data creation
-NUMDOCS = 50
-MIN_DOC_LENGTH = 25
-MAX_DOC_LENGTH = 50
+words2 = ['A':'E';]
 
-P_HOT_DOC = 0.5
-P_KEY_PHRASE_1 = 0.05
-P_KEY_PHRASE_0 = 0.01
+wordsdist1 = CreateWordDistibution(words)
+wordsdist2 = CreateWordDistibution(words)
+
+X = SynthParams(50, 25, 25, 0.5, 0.05, 0.01, wordsdist1, wordsdist2)
+
+Y = createSynthData(X, words)
+WriteData("synthdata.txt", "synthcrps.txt", Y)
 
 
 
 
-synthdocs = Vector{Tuple{Int8, String}}()
-num_hot = 0
-for i in 1:NUMDOCS
+function createSynthData(X, vocab)
+    synthdocs = Vector{Tuple{Bool, String}}()
+    for i in 1:X.NUMDOCS
+        currentDoc = ""
+        hotdoc = rand() < X.P_HOT_DOC
+        currWord = ""
 
-    #initialize current doc being created to not be one of interest
-    currentdoc = ""
-    hotdoc = 0
+        currentDocLength = rand(X.MIN_DOC_LENGTH:X.MAX_DOC_LENGTH, 1)[1]
 
-    #Pick a random number between max and min doc length
-    currentdoclength = rand(MIN_DOC_LENGTH:MAX_DOC_LENGTH, 1)[1]
-
-    #decides whether or not current doc will be doc of interest
-    #based on chosen probability
-    if rand() < P_HOT_DOC
-        hotdoc = 1
-
-        for j in 1:currentdoclength
-            if rand() < P_KEY_PHRASE_1
-                #put space at end of anything you concat to doc
-                currentdoc *= "A M D "
+        j = 1
+        while j <= currentDocLength
+            if j == 1
+                currWord = rand(vocab, 1)[1]
+                currentDoc *= currWord * " "
             else
-                currentdoc *= String(rand(words, 1)) * " "
+                prevWord = currWord
+
+                prob = rand()
+                if hotdoc
+                    currWord = sampleFromWords(prob, prevWord, X.DIST1, vocab)
+                else
+                    currWord = sampleFromWords(prob, prevWord, X.DIST2, vocab)
+                end
+
+                currentDoc *= currWord * " "
+            end
+            j += 1
+        end
+        push!(synthdocs, (hotdoc, currentDoc))
+    end
+    return synthdocs
+end
+
+function WriteData(filename, crpsfile, data)
+    open(filename, "w") do io
+        for d in data
+            write(io, string(1*d[1]) * " ")
+            write(io, d[2])
+            write(io, '\n')
+        end
+    end
+
+
+    open(crpsfile, "w") do io
+        for d in data
+            write(io, d[2])
+            write(io, '\n')
+        end
+    end
+end
+
+
+function sampleFromWords(p, prev, distDict, vocab)
+    chosenNext = 'a'
+    lower = 0
+
+    for c in vocab
+
+        if p < distDict[prev][c] + lower && distDict[prev][c] != 0
+            return c
+        else
+            lower += distDict[prev][c]
+        end
+    end
+    return chosenNext
+end
+
+
+
+function CreateWordDistibution(vocab, strong)
+    dists = Dict{Char, Dict{Char, Float64}}()
+    for w in vocab
+        dists[w] = Dict(w=>0)
+
+        
+        probs = rand(length(vocab) - 1)
+        probs = probs ./ sum(probs)
+
+        i = 1
+        for x in vocab
+            if x == w
+                dists[w][x] = 0
+            else
+                dists[w][x] = probs[i]
+                i += 1
             end
         end
-    else
-        for j in 1:currentdoclength
-            currentdoc *= String(rand(words, 1)) * " "
-        end
     end
-
-    #Alternate for embeddings
-
-    num_hot += hotdoc
-    println(i)
-    push!(synthdocs, (hotdoc, currentdoc))
-
+    return dists
 end
 
-#Write synthetic data to txt file
+word2vec("synthcrps.txt", "synthvectors.txt")
 
-open("src/Jack/synthdata.txt", "w") do io
-    for d in synthdocs
-        write(io, string(d[1]) * " ")
-        write(io, d[2])
-        write(io, '\n')
-    end
-end
+m = wordvectors("synthvectors.txt")
+similarity(m, "A", "Z")
