e3aedf64d69c4553c6814a3fb1679b8c1ac4908f
jackferrell <jferrel3@ncsu.edu>
2021-06-21 14:43:30 -0400

    "fixed" my model

--- src/Jack/jackModel.jl
+++ src/Jack/jackModel.jl
@@ -6,103 +6,124 @@ using Plots
 include("../data_cleaning.jl")
 include("../embeddings_nn.jl")
 
-#Importing data and creating corpus
-data = importClean()
-sort!(data, "medical_specialty")
-rm("corpus.txt")
-createCorpusText(data, 0)
+vecLength1 = 15
+window1 = 500
 
-#Create embeddings
-vecfile = @__DIR__
-vecfile = vecfile * "/vectors.txt"
+trainTestSplitPercent = .9
+η = 0.05
+epochs = 2000
 
-veclength = 15
-word2vec("corpus.txt", vecfile; size = veclength, window = 15, min_count = 5)
-
-#Extract embeddings
-embeddings = wordvectors(vecfile)
+paddingChoice = 1
+batchsize_custom = 100
 
 
+true_data = importClean()
+sort!(true_data, "medical_specialty")
+createCorpusText(true_data, paddingChoice)
+
+# Medical field we're training on
 field = " Cardiovascular / Pulmonary"
-#Balance field of interest with negative samples
-sample = filtration(data, field)
-#Split into training and testing
-train, test =  TrainTestSplit(sample, 0.9)
 
-yTrain = train[:, 1] .== field
-yTrain *= 1.0
-yTest = test[:, 1] .== field
-yTest *= 1.0
+# Creating the embeddings using Word2Vec
+word2vec("corpus.txt", "vectors.txt", size = vecLength1, verbose = true, window = window1)
+M = wordvectors("vectors.txt", normalize = false)
+rm("vectors.txt")
+rm("corpus.txt")
+
+# Filtering new data and splitting train/test
+datasub = filtration(true_data, field)
+data, test = TrainTestSplit(datasub, trainTestSplitPercent)
 
 
-#Put embeddings into matrix form
+# Making the classification column for after splitting
+class = data[:,1] .== field
+class = class * 1.0
 
-vecsTrain = zeros(length(yTrain), veclength)
-vecsTest = zeros(length(yTest), veclength)
+classTest = test[:, 1] .== field
+classTest = classTest * 1.0
 
-for i in 1:length(yTrain)
-    vecsTrain[i, :] = formulateText(embeddings, train[i, 3])
+tmat = Matrix(test)
+
+# Creating an empty matrix for training where the length size is
+# double the medical field chosen and the width is the sum of two
+# vectors lengths (or one vector if using one)
+vecsTrain = zeros(length(class),vecLength1)
+vecsTest = zeros(length(tmat[:, 1]), vecLength1)
+
+# Places embeddings in empty matrix
+for i in 1:length(class)
+    vecsTrain[i,:] = formulateText(M,data[i,3])
 end
 
-for i in 1:length(yTest)
-    vecsTest[i, :] = formulateText(embeddings, test[i, 3])
+for i in 1:length(tmat[:, 1])
+    vecsTest[i,:] = formulateText(M, test[i,3])
 end
 
+# creating the matrix to run through nn
 train_mat = vecsTrain'
 test_mat = vecsTest'
 
+# Makes "DataLoader" classes for both testing and training data
+# Batchsize for training shoulde be < ~size(train). Way less
+newTestData = Flux.Data.DataLoader((test_mat, classTest'))
+newTrainingData = Flux.Data.DataLoader((train_mat, class'),
+                                    batchsize = batchsize_custom, shuffle = true)
 
-#Make matrices into flux format
-trainingdata = Flux.Data.DataLoader((train_mat, yTrain'), batchsize = 100, shuffle = true)
-testingdata = Flux.Data.DataLoader((test_mat, yTest'))
+nn = Chain(Dense(15, 7, relu),
+            Dense(7, 4, relu),
+            Dense(4, 1, x->σ.(x)))
 
-#Train first model with hardsigmoid
-model1 = Chain(
-            Dense(15, 7, hardsigmoid),
-            Dense(7, 1, x -> σ.(x))
-        )
+opt = Flux.Optimiser(ExpDecay(), RADAM(0.001))
 
-opt = Descent(0.05)
+loss(x, y) = sum(Flux.Losses.binarycrossentropy(nn(x), y))
 
-loss(x, y) = sum(Flux.Losses.binarycrossentropy(model1(x), y))
-para = Flux.params(model1)
-
-epoch = 1000
-
-for i in 1:epoch
-    Flux.train!(loss, para, trainingdata, opt)
-end
-
-Flux.params(model1)
-
-acc = 0
-pred = Vector{Float64}()
-for (x, y) in testingdata
-    #Note: although broadcasting is being used here,
-    #these are not vectors, they are matrices with single values
-    global acc += sum((model1(x) .> .5) .== y)
-    push!(pred, model1(x)[1, 1])
-end
-print(1- acc / length(yTest))
-
-#Save model
-using BSON: @save
-
-@save "src/Jack/jackModel.bson" model1
-
-r = MLBase.roc(convert(BitVector, yTest), pred, 100)
-fpr = false_positive_rate.(r)
-tpr = true_positive_rate.(r)
-
-plot(fpr, tpr)
-
-function sumEmbeddings(embeddings, transcript)
-    words = split(transcript, " ")
-    scriptvec = zeros(length(get_vector(embeddings, "the")))
-    for word in words
-        if word ∉ stopwords(Languages.English()) && word ∈ embeddings.vocab
-            scriptvec += get_vector(embeddings, word)
+totalLoss = []
+traceY = []
+traceY2 = []
+epochs = 1000
+ps = Flux.params(nn)
+    for i in 1:epochs
+        Flux.train!(loss, ps, newTrainingData, opt)
+        if i % 100 == 0
+            println(i)
         end
+        for (x,y) in newTrainingData
+            totalLoss = loss(x,y)
+        end
+        push!(traceY, totalLoss)
+        acc = 0
+        for (x,y) in newTestData
+            acc += sum((nn(x) .> .5) .== y)
+        end
+        decimalError = 1 - acc/length(classTest)
+        percentError = decimalError * 100
+        percentError = round(percentError, digits=2)
+        push!(traceY2, percentError)
     end
-    return scriptvec
-end
+
+# Testing for accuracy (at the end)
+acc = 0
+    for (x,y) in newTestData
+        acc += sum((nn(x) .> .5) .== y)
+    end
+
+    decimalError = 1 - acc/length(classTest)
+    percentError = decimalError * 100
+    percentError = round(percentError, digits=2)
+    print("Error Rate: ")
+    print(percentError)
+    println("%")
+
+x = 1:epochs
+y = traceY
+plot(x, y, label = "Loss Progression")
+xlabel!("Total # of epochs")
+ylabel!("Loss values")
+
+x2 = 1:epochs
+y2 = traceY2
+plot(x2, y2)
+xlabel!("Total # of epochs")
+ylabel!("Error Rate")
+
+print(y2[argmin(y2)])
