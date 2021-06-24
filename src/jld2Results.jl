df9ff4ceac24792c78c4e085b83aaa62475afcfc
eplanch <eplanch@ncsu.edu>
2021-06-24 18:38:17 -0400

    file reader

--- src/jld2Results.jl
+++ src/jld2Results.jl
@@ -6,9 +6,9 @@ function jld2Results(errors, preds, trues)
     t = []
     for i in 1:1000
         if get(errors, "run"*string(i),0) != 0
-            push!(err, 1-sum(get(preds, "run"*string(i),0) .== get(trues, "run"*string(i),0))/1000)
-            push!(p, get(preds, "run"*string(i),0))
-            push!(t, get(trues, "run"*string(i),0))
+            push!(err, get(errors, "run"*string(i),0)[1])
+            push!(p, get(preds, "run"*string(i),0)[1])
+            push!(t, get(trues, "run"*string(i),0)[1])
         end
     end
     print(length(err))
@@ -18,9 +18,11 @@ function jld2Results(errors, preds, trues)
     p[best],
     t[best],
     err[worst],
-    p[worst], 
+    p[worst],
     t[best],
     mean(err)]
 end
 
-jld2Results(JLD.load("PCALR/ErrorsFinal.jld"),JLD.load("PCALR/PredsFinal.jld"),JLD.load("PCALR/TruesFinal.jld"))
+eb,pb,tb,ew,pw,tw,ea = jld2Results(JLD.load("/Users/eplanch/Documents/GitHub/nlpdrums/src/emi/ErrorsFinal.jld"),
+            JLD.load("/Users/eplanch/Documents/GitHub/nlpdrums/src/emi/PredsFinal.jld"),
+            JLD.load("/Users/eplanch/Documents/GitHub/nlpdrums/src/emi/TruesFinal.jld"))
