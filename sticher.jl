fdb37e34e792347ae4bfdbf5de02caaf5e3d7ca6
eplanch <eplanch@ncsu.edu>
2021-06-24 11:45:49 -0400

    *** STICHER FINAL ***

--- sticher.jl
+++ sticher.jl
@@ -6,13 +6,13 @@ JLD.save("ErrorsFinal.jld", "run0", 0)
 
 for i in 1:1000
     jldopen("PredsFinal.jld", "r+") do file
-        write(file, "Run" * string(n), "run"*string(i), JLD.load("Preds" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
+        write(file, "run"*string(i), JLD.load("Preds" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
     end
     jldopen("TruesFinal.jld", "r+") do file
-        write(file, "Run" * string(n), "run"*string(i), JLD.load("Trues" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
+        write(file, "run"*string(i), JLD.load("Trues" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
     end
     jldopen("ErrorsFinal.jld", "r+") do file
-        write(file, "Run" * string(n), "run"*string(i), JLD.load("Errors" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
+        write(file, "run"*string(i), JLD.load("Errors" *string(i)* ".jld", "val"))  # alternatively, say "@write file A"
     end
     rm("Preds" *string(i)* ".jld")
     rm("Trues" *string(i)* ".jld")
