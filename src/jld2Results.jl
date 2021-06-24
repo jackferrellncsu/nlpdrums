8292c4e2ce8a1c4f29b720f1c478636770746a36
mlovig <mlovig@ncsu.edu>
2021-06-24 18:30:22 -0400

    Update jld2Results.jl

--- src/jld2Results.jl
+++ src/jld2Results.jl
@@ -1,13 +1,26 @@
+using JLD
+
 function jld2Results(errors, preds, trues)
     err = []
+    p = []
+    t = []
     for i in 1:1000
         if get(errors, "run"*string(i),0) != 0
-            push!(err, get(errors, "run"*string(i),0))
-            push!(preds, get(preds, "run"*string(i),0))
-            push!(trues, get(trues, "run"*string(i),0))
+            push!(err, 1-sum(get(preds, "run"*string(i),0) .== get(trues, "run"*string(i),0))/1000)
+            push!(p, get(preds, "run"*string(i),0))
+            push!(t, get(trues, "run"*string(i),0))
         end
     end
-    best = argmin(errors)
-    worst = argmax(errors)
-    return [err[best], preds[best], trues[best], err[worst], preds[worst], trues[best]]
+    print(length(err))
+    best = argmin(err)
+    worst = argmax(err)
+    return [err[best],
+    p[best],
+    t[best],
+    err[worst],
+    p[worst], 
+    t[best],
+    mean(err)]
 end
+
+jld2Results(JLD.load("PCALR/ErrorsFinal.jld"),JLD.load("PCALR/PredsFinal.jld"),JLD.load("PCALR/TruesFinal.jld"))
