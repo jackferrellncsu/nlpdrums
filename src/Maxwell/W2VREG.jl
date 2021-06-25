38f252d994b1eba2d56492b8c075385367ffec78
mlovig <mlovig@ncsu.edu>
2021-06-25 09:30:33 -0400

    Update W2VREG.jl

--- src/Maxwell/W2VREG.jl
+++ src/Maxwell/W2VREG.jl
@@ -40,7 +40,55 @@ vecLength2 = 0
 #Max is 50
 window1 = 50
 
-M = wordvectors("vectors0.txt", normalize = false)
+function createCorpusText(data, choice)
+   allDocs = ""
+   thePad = ""
+   for i in 1:3000
+      thePad = thePad * " randomWordNow"
+   end
+   for i in 1:length(data[:, 3])
+      println(i)
+      if choice == 1
+         if i != 1
+            if data[i, 1] != data[i-1, 1]
+               println("This is a seperation")
+               allDocs = allDocs * thePad * " " * data[i, 3]
+            else
+               allDocs = allDocs * " " * data[i, 3]
+            end
+         end
+      elseif choice == 0
+         allDocs = allDocs * " " * data[i, 3]
+      elseif choice == 2
+         allDocs = allDocs * thePad * " " * data[i, 3]
+      end
+   end
+   open("corpus.txt","a") do io
+      println(io,allDocs)
+   end
+end
+
+function importClean()
+    filename = "cleanedData.csv"
+    filepath = joinpath(@__DIR__, filename)
+    arr = CSV.read(filepath, DataFrame)
+
+    return arr
+end
+
+function filtration(df, field)
+   indexes = []
+   for i in 1:length(df[:,1])
+      if df[i,1] == field
+         push!(indexes,i)
+      else
+         if rand() < sum(df[:,1].==field)/(length(df[:,1]) - sum(df[:,1].==field))
+            push!(indexes,i)
+         end
+      end
+   end
+   return df[indexes,:]
+end
 
 function formulateText(model, script)
    words = split(script, " ")
@@ -53,19 +101,40 @@ function formulateText(model, script)
    return vecs
 end
 
+field = " Cardiovascular / Pulmonary"
 n = parse(Int64, get(parsed_args, "arg1", 0 ))
-datatot = CSV.read("JonsData.csv", DataFrame)[(n-1)*1000+1:n*1000,:]
 Random.seed!(n)
-data, test = TrainTestSplit(datatot, .9);
+D = CSV.read("/Users/mlovig/Documents/GitHub/nlpdrums/src/cleanedData.csv", DataFrame)
+Random.seed!(n)
+train, test = TrainTestSplit(D, .9);
 
-vecs = zeros(size(data)[1],vecLength1 + vecLength2)
+Random.seed!(n)
+data = filtration(train, " Cardiovascular / Pulmonary")
+createCorpusText(train,0)
+
+#defining the lengths of the syntanctic or topical embeddings
+vecLength1 = 15
+
+#Defining the window sizes
+window1 = 50
+
+#Creating the syntactic vector
+word2vec("corpus.txt", "vectors.txt", size = vecLength1, verbose = true, window = window1, negative = 10, min_count = 0)
+   M = wordvectors("vectors.txt", normalize = true)
+
+   rm("vectors.txt")
+   #creting the topical vectors
+
+rm("corpus.txt")
+
+vecs = zeros(size(data)[1],vecLength1)
 for i in 1:size(data)[1]
     vecs[i,:] = formulateText(M,data[i,1])
 end
 
 df = DataFrame(hcat(vecs,data[:,2]))
 
-ii = vecLength1 + vecLength2
+ii = vecLength1
 z=term(Symbol(:x, ii+1)) ~ sum(term.(Symbol.(names(df[:, Not(Symbol(:x, ii+1))]))))
 logit = glm(z,df, Bernoulli(), LogitLink())
 
@@ -86,6 +155,6 @@ rez = preds.>.5
 
 errors = 1 - sum(rez .== classtest)/length(rez)
 
-save("Preds" * string(n) * ".jld", "val", preds)
-save("Trues" * string(n) * ".jld", "val", classtest)
-save("Errors" *string(n) * ".jld", "val", errors)
+JLD.save("Preds" * string(n) * ".jld", "val", preds)
+JLD.save("Trues" * string(n) * ".jld", "val", classtest)
+JLD.save("Errors" *string(n) * ".jld", "val", errors)
