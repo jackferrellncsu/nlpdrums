2a799003782d2907de805e8ae1ac308914ffbab1
mlovig <mlovig@ncsu.edu>
2021-07-20 09:40:32 -0400

    stuff

---
+++ src/Maxwell/redactedPOS.jl
@@ -0,0 +1,54 @@
+using SparseArrays
+using SparseArrayKit
+using JLD
+using Word2Vec
+using LinearAlgebra
+using Statistics
+using Embeddings
+using Flux
+using Random
+using DataFrames
+using Lathe.preprocess: TrainTestSplit
+using Plots
+using StatsBase
+using BSON
+using ProgressBars
+using CSV
+using Transformers
+using Transformers.Basic
+using Transformers.Pretrain
+
+# ------------------------ Data Cleaning ------------------------ #
+
+include("pride_functions2.jl")
+
+# Reading in text file
+brown_df = CSV.read("brown.csv", DataFrame)
+sentences = brown_df[5]
+tags = brown_df[6]
+
+sentences = split.(sentences, " ")
+tags = split.(tags, " ")
+
+function word_masker(sentences, tags)
+
+    act_word = []
+    act_pos = []
+    for i in 1:length(sentences)
+        samp = sample(1:length(sentences[i]))
+        push!(act_word, sentences[i][samp])
+        push!(act_pos, tags[i][samp])
+        sentences[i][samp] = "/MASK/"
+    end
+    return act_word, act_pos, sentences
+end
+
+Random.seed!(123)
+act_word, act_pos, new_sentences = word_masker(sentences, tags)
+
+ENV["DATADEPS_ALWAYS_ACCEPT"] = true
+
+bert_model, wordpiece, tokenizer = pretrain"bert-uncased_L-12_H-768_A-12"
+vocab = Vocabulary(wordpiece)
+
+BERTEMB = makeDataByWord(S,N,bert_model, wordpiece, tokenizer, vocab, uni)
