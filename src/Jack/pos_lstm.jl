6311caa077ea6d47fdf73d65a0cb3c6bf86e934b
jackferrell <jferrel3@ncsu.edu>
2021-07-20 00:26:39 -0400

    Update pos_lstm.jl

--- src/Jack/pos_lstm.jl
+++ src/Jack/pos_lstm.jl
@@ -14,3 +14,11 @@ unique_words = get_uniques(sentences)
 
 #embtable = load_embeddings(GloVe{:en}, 6, keep_words = unique_words)
 #JLD.save("posEmbs.jld", "embtable", embtable)
+
+#embtable and useful dicts
+embtable = JLD.load("Data/posEmbs.jld", "embtable")
+vector_from_word = Dict(word=>embtable.embeddings[:,ii] for (ii,word) in enumerate(embtable.vocab))
+word_from_vector = Dict(embtable.embeddings[:, ii] => word for (ii, word) in enumerate(embtable.vocab))
+
+#LSTM will have forwards and backwards, each with input 300, output size of pos_tags
+
