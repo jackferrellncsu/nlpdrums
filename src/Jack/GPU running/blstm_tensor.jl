0a11f56c146dedb1512eab5f384238edd581ff71
jackferrell <jferrel3@ncsu.edu>
2021-07-24 11:37:06 -0400

    tinkering

---
+++ src/Jack/GPU running/blstm_tensor.jl
@@ -0,0 +1,174 @@
+using SparseArrays
+using SparseArrayKit
+using JLD
+using Word2Vec
+using LinearAlgebra
+using Statistics
+using Embeddings
+using Flux
+using Flux: Losses
+using Random
+using DataFrames
+using Lathe.preprocess: TrainTestSplit
+using Plots
+using StatsBase
+using BSON
+using ProgressBars
+using CSV
+
+# ------------------------ Data Cleaning ------------------------ #
+
+include("brown_functions.jl")
+
+# Reading in text file
+brown_df = CSV.read("brown.csv", DataFrame)
+brown_data = brown_df[4]
+raw_sentences = split.(brown_data, " ")
+
+# Finding unique words and embeddings for each
+unique_pos, sentences, sentence_tags = data_cleaner(raw_sentences)
+words = get_word_vec(sentences)
+unique_words = convert(Vector{String},unique(words))
+
+# Finding embeddings for each unique word
+#embeddings_glove = load_embeddings(GloVe{:en},4, keep_words=Set(unique_words))
+#embtable = Dict(word=>embeddings_glove.embeddings[:,ii] for (ii,word) in enumerate(embeddings_glove.vocab))
+embtable = JLD.load("brownEmbs.jld", "embtable")
+# Finding the words that have GloVe embeddings
+keys_embtable = get_keys(embtable)
+
+# Finding the words that don't have GloVe embeddings
+no_embeddings = setdiff(unique_words, keys_embtable)
+
+sent_tens, new_sent, new_tags = sent_embeddings(sentences, sentence_tags, 300, 180, embtable)
+
+new_sent = convert(Vector{Vector{String}}, new_sent)
+new_tags = convert(Vector{Vector{String}}, new_tags)
+sent_tens = convert(Array{Float32, 3}, sent_tens)
+
+# Masks random word in each sentence
+masked_word, masked_pos, new_sentences = word_masker(new_sent, new_tags)
+
+# mask_ind - the index of the masked word in each sentence
+# mask_emb - the embedding of each masked word (NOT NEEDED)
+mask_ind, mask_emb, sent_tens_emb = create_embeddings(masked_word, masked_pos,
+                                new_sentences, sent_tens, embtable)
+
+sent_tens_emb = create_window(sent_tens_emb, 15)
+sent_tens_emb = convert(Array{Float32, 3}, sent_tens_emb)
+
+#onehot_mat = Flux.onehotbatch(masked_pos, unique_pos)
+onehot_vecs = zeros(length(unique_pos), length(masked_pos))
+for i in 1:length(masked_pos)
+    onehot_vecs[:, i] = Flux.onehot(masked_pos[i], unique_pos)
+end
+onehot_vecs = convert(Array{Float32, 2}, onehot_vecs)
+
+
+
+temp_train, test, temp_train_class, test_class = SampleMats(sent_tens_emb, onehot_vecs) |> gpu
+train, calib, train_class, calib_class = SampleMats(temp_train, temp_train_class) |> gpu
+
+
+
+# Creating DataLoader
+dl_calib = Flux.Data.DataLoader((calib, calib_class))
+dl_test = Flux.Data.DataLoader((test, test_class))
+dl_train = Flux.Data.DataLoader((train, train_class),
+                                    batchsize = 100, shuffle = true)
+
+
+#data = collect(zip(train, train_class))
+
+forward = LSTM(300, 150) |> gpu
+backward = LSTM(300, 150) |> gpu
+embedding = Dense(300, 300)|> gpu
+predictor = Chain(Dense(300, 250, relu), Dense(250,190), softmax)|> gpu
+
+function BLSTM(x)
+
+    #Flux.reset!((forward, backward))
+    fw = forward.([x[:, 1:15, i] for i in 1:size(x, 3)])
+    fw_mat = hcat.(f[:,15] for f in fw)
+
+    bw = backward.([x[:, end:-1:17, i] for i = size(x, 3):-1:1])
+    bw_mat = hcat.(b[:,15] for b in bw)
+
+    fw_temp = fw_mat[1]
+    for i in 2:length(fw_mat)
+        fw_temp = hcat(fw_temp, fw_mat[i])
+    end
+
+    bw_temp = bw_mat[1]
+    for i in 2:length(bw_mat)
+        bw_temp = hcat(bw_temp, bw_mat[i])
+    end
+    #@show fw_temp
+    res = vcat(fw_temp, bw_temp)
+    #@show res
+    return res
+end
+
+vectorizer(x) = embedding(BLSTM(x))
+
+model(x) = predictor(vectorizer(x))
+
+# Tester
+for (x, y) in dl_train
+    #c = BLSTM(x)
+    d = model(x)
+    @show d
+    l = loss(x, y)
+    #@show typeof(c)
+    #@show size(c)
+    #@show c
+    #d = embedding(c)
+    #@show typeof(d)
+    #@show size(d)
+    ##@show d
+    #e = predictor(d)
+    #@show typeof(e)
+    #@show size(e)
+    ##@show e
+    #@show crossentropy(e[:,1], y[:,1])
+
+end
+
+
+# Optimizer
+opt = RADAM()
+
+# Parameters
+ps = Flux.params((forward, backward, embedding, predictor))
+
+# Loss
+function loss(x, y)
+    l = sum(crossentropy(model(x), y))
+    Flux.reset!((forward, backward))
+    return l
+end
+
+# Training the Neural Net, Tracking Loss Progression
+epochs = 1
+traceY = []
+
+#evalcb() = push!(traceY, loss(train[:, :, 100]), train_class[:, 100]))
+throt = Flux.throttle(evalcb, 5)
+
+for i in ProgressBar(1:epochs)
+    Flux.train!(loss, ps, dl_train, opt)
+    #Flux.reset!((forward, backward))
+end
+
+# Plots Loss
+plotly()
+x = 1:epochs
+y = traceY
+plot(x, y)
+
+
+function predict(x)
+    pred = model1(x)
+    Flux.reset!((forward, backward))
+    return pred
+end
---
+++ src/Jack/GPU running/brown_functions.jl
@@ -0,0 +1,258 @@
+using SparseArrays
+using SparseArrayKit
+using JLD
+using Word2Vec
+using LinearAlgebra
+using Statistics
+using Embeddings
+using Flux
+using Random
+using DataFrames
+using Lathe.preprocess: TrainTestSplit
+using Plots
+using StatsBase
+using BSON
+
+# ------------------------ Functions ------------------------ #
+"""
+    word_masker(sentences, tags)
+
+Mask a randomly selected word in every sentence in sentences.
+
+Return the
+"""
+function word_masker(sentences, tags)
+
+    act_word = []
+    act_pos = []
+    for i in 1:length(sentences)
+        samp = sample(1:length(sentences[i]))
+        push!(act_word, sentences[i][samp])
+        push!(act_pos, tags[i][samp])
+        sentences[i][samp] = "/MASK/"
+    end
+    return act_word, act_pos, sentences
+end
+
+function data_cleaner(raw_sentences)
+
+    raw_tags = []
+    raw_words = []
+    for sent in raw_sentences
+        raw_tags_temp = []
+        raw_words_temp = []
+        for word in sent
+            ind = findlast(x -> x == '/', word)
+            pos = word[ind+1:end]
+            pos = replace(pos, "-tl" => "")
+            pos = replace(pos, "-hl" => "")
+            pos = replace(pos, "fw-" => "")
+            pos = replace(pos, "-nc" => "")
+            pos = replace(pos, "bez" => "bbb")
+            push!(raw_tags_temp, convert(String, pos))
+            push!(raw_words_temp, lowercase(word[1:ind-1]))
+        end
+        push!(raw_tags, raw_tags_temp)
+        push!(raw_words, raw_words_temp)
+    end
+
+    individual_tags = []
+    for i in 1:length(raw_tags)
+        for j in 1:length(raw_tags[i])
+            push!(individual_tags, (raw_tags[i][j]))
+        end
+    end
+
+    tagger = unique!(individual_tags)
+
+    return tagger, raw_words, raw_tags
+end
+
+function get_word_vec(sentences)
+
+    words = Vector{String}()
+    for i in 1:length(sentences)
+        for j in 1:length(sentences[i])
+            push!(words, sentences[i][j])
+        end
+    end
+    return words
+end
+
+function get_keys(dict)
+
+    keyz = []
+    for i in keys(dict)
+        push!(keyz, i)
+    end
+    return keyz
+end
+
+function sent_embeddings(sentences, sentence_tags, num_embed, num_words, dict)
+
+    # Embeddings for each sentence
+    tens = zeros(num_embed, num_words, length(sentences))
+
+    sent = []
+    tags = []
+    for i in 1:length(sentences)
+        temp = []
+        temp_tag = []
+        for j in 1:length(sentences[i])
+            if length(sentences[i]) < num_words
+                tens[:, j, i] = get(dict, sentences[i][j], zeros(num_embed))
+                push!(temp, sentences[i][j])
+                push!(temp_tag, sentence_tags[i][j])
+            elseif length(sentences[i]) >= num_words
+                if j == (num_words + 1)
+                    break
+                else
+                    tens[:, j, i] = get(dict, sentences[i][j], zeros(num_embed))
+                    push!(temp, sentences[i][j])
+                    push!(temp_tag, sentence_tags[i][j])
+                end
+            end
+        end
+        # For pre-sentences with < "num_words", average of word embeddings
+        # is taken and added to tensor
+        if length(sentences[i]) < num_words
+
+            vecs_needed = 0
+            vecs_needed = num_words - length(sentences[i])
+
+            for j in 1:vecs_needed
+                tens[:, length(sentences[i]) + j, i] .= 0.0
+            end
+        end
+        push!(sent, temp)
+        push!(tags, temp_tag)
+    end
+    return tens, sent, tags
+end
+
+function create_embeddings(masked_word, masked_pos, new_sentences, sent_tens, dict)
+
+    # Finds indices of masked words for each sentence
+    masked_ind = []
+    for i in 1:length(new_sentences)
+        for j in 1:length(new_sentences[i])
+            if new_sentences[i][j] == "/MASK/"
+                push!(masked_ind, j)
+            end
+        end
+    end
+
+    # Embeddings of masked words, not needed
+    masked_embeddings = Vector{Vector{Float32}}()
+    for i in 1:length(masked_word)
+        push!(masked_embeddings, get(dict, masked_word[i], zeros(300)))
+    end
+
+    # Embeddings for each sentence
+    for i in 1:length(sent_tens[1, 1, :])
+        temp = []
+        mask = masked_ind[i]
+        sent_tens[:, mask, i] = fill(-20.0, 300)
+    end
+
+
+    return masked_ind, masked_embeddings, sent_tens
+end
+
+function splitter(sent_emb, onehot_vecs, train_test, train_calib)
+
+    # Computing sizes of each set
+    first_train_size = Int(ceil(Base.length(sent_emb) .* train_test))
+    test_size = Int(Base.length(sent_emb) - first_train_size)
+    train_size = Int(ceil(first_train_size * train_calib))
+    calib_size = Int(first_train_size - train_size)
+
+    train = []
+    train_class = []
+    train_ind = sample(1:Base.length(sent_emb), train_size, replace = false)
+    for i in 1:length(train_ind)
+        push!(train, sent_emb[train_ind[i]])
+        push!(train_class, onehot_vecs[train_ind[i]])
+    end
+    train = convert(Vector{Vector{Vector{Float32}}}, train)
+    train_class = convert(Vector{Vector{Float32}}, train_class)
+
+    test = []
+    test_class = []
+    test_ind = sample(1:Base.length(sent_emb), test_size, replace = false)
+    for i in 1:length(test_ind)
+        push!(test, sent_emb[test_ind[i]])
+        push!(test_class, onehot_vecs[test_ind[i]])
+    end
+    test = convert(Vector{Vector{Vector{Float32}}}, test)
+    test_class = convert(Vector{Vector{Float32}}, test_class)
+
+
+    calib = []
+    calib_class = []
+    calib_ind = sample(1:Base.length(sent_emb), calib_size, replace = false)
+    for i in 1:length(calib_ind)
+        push!(calib, sent_emb[test_ind[i]])
+        push!(calib_class, onehot_vecs[calib_ind[i]])
+    end
+    calib = convert(Vector{Vector{Vector{Float32}}}, calib)
+    calib_class = convert(Vector{Vector{Float32}}, calib_class)
+
+    return train, train_class, test, test_class, calib, calib_class
+end
+
+function SampleMats(x_mat, y_mat, prop = 0.9)
+    inds = [1:size(x_mat)[3];]
+    length(inds)
+    trains = sample(inds, Int(floor(length(inds) * prop)), replace = false)
+    inds = Set(inds)
+    trains = Set(trains)
+    tests = setdiff(inds, trains)
+
+    train_x = x_mat[:, :, collect(trains)]
+    train_y = y_mat[:, collect(trains)]
+
+    test_x = x_mat[:, :, collect(tests)]
+    test_y = y_mat[:, collect(tests)]
+
+
+    return train_x, test_x, train_y, test_y
+end
+
+"""
+    find_mask_ind(tens)
+
+Takes in a tensor, returns a vector of mask index for each sentence in tensor.
+Assumes mask embeddings are set to -20.0.
+"""
+function find_mask_ind(tens)
+
+    ind = []
+    for i in 1:length(tens[1, 1, :])
+        push!(ind, argmin(tens[1, :, i]))
+    end
+    return ind
+end
+
+function create_window(sent_tens_emb, window_size)
+
+    mask_ind = 0
+    num_wind = (window_size * 2) + 1
+    new_tens = zeros(300, num_wind, length(sent_tens_emb[1, 1, :]))
+    for i in 1:length(sent_tens_emb[1, 1, :])
+        mask_ind = argmin(sent_tens_emb[1, :, i])
+        if mask_ind == 16
+            new_tens[:, :, i] = sent_tens_emb[:, 1:num_wind, i]
+        elseif mask_ind > 16
+            num_start = (mask_ind - 16) + 1
+            num_end = num_wind + (num_start - 1)
+            new_tens[:, :, i] = sent_tens_emb[:, num_start:num_end, i]
+        elseif mask_ind < 16
+            num_zero = window_size - (mask_ind - 1)
+            new_mat = zeros(300, num_zero)
+            stopper = mask_ind + (window_size)
+            new_tens[:, :, i] = hcat(new_mat, sent_tens_emb[:, 1:stopper, i])
+        end
+    end
+    return new_tens
+end
