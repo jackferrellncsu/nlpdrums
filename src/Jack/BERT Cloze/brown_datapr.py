0b4ca61babc3d92b34af47444c515b0a329ba8e8
jackferrell <jferrel3@ncsu.edu>
2021-07-27 20:38:44 -0400

    pyFUN

--- src/Jack/BERT Cloze/brown_datapr.py
+++ src/Jack/BERT Cloze/brown_datapr.py
@@ -1,21 +1,55 @@
+from json import load
 import pandas as p
 import os
+import random
+from sklearn.model_selection import train_test_split
 
 
-def prepare_data():
+#load in sentences df
+#Not put back together yet, in (pre-bert) token form
+def load_sentencesdf():
     raw_data = p.read_csv("src/Jack/Data/brown.csv")
     sentences = raw_data["raw_text"].str.split(expand=True)
     sentences = sentences.apply(lambda x: x.str.rsplit("/").str[0])
-    clean_sentences = sentences.apply(lambda x: x.str.cat(sep=" "), axis = 1)
-    entire_corpus = clean_sentences.str.cat(sep=" [SEP] ")
-    return entire_corpus
+    sentences.insert(180, "mask_ind", None)
+    return sentences
+    
 
-z = prepare_data()
+#Get index we will later mask, only called in 
+def get_mask_helper(row):
+    #end and beginning offset by one due to [CLS] token when making bert embeddings
+    range = row.notna().sum()+1
+    ind = random.randrange(1, range)
 
-os.getcwd(__file__)
-import inspect
+    return ind
 
 
-with open("src/Jack/Data/brown_string.txt", "w") as text_file:
-    text_file.write(z)
+#puts sentences back together, returns a list of them
+def whole_sentences(sentence_df):
+    k = clean_sentences = sentences.apply(lambda x: x.str.cat(sep=" "), axis = 1)
+    return clean_sentences
+    
+#gets index we will mask for each sentence
+#one word is masked in each sentence 
+def get_masked_inds(sentences):
+    return sentences.apply(lambda x: get_mask_helper(x), axis = 1)
+    
+#Apply functions
+sentences = load_sentencesdf()
+clean_sentences = whole_sentences(sentences)
+clean_sentences.head()
+mask_inds = get_masked_inds(sentences)
+mask_inds.head()
 
+#Create new DF with cleaned sentences and masked inds
+new_df = p.DataFrame([clean_sentences, mask_inds]).transpose()
+new_df.head()
+
+train_cal, test = train_test_split(new_df, test_size=0.25, shuffle=True)
+train, cal = train_test_split(train_cal, test_size=0.25, shuffle=True)
+
+#Save new dfs with sentences and masked inds as tab seperated CSV
+new_df.to_csv("src/Jack/Data/brown_master.csv", sep = "\t", index = False)
+train.to_csv("src/Jack/Data/brown_train.csv", sep = "\t", index = False)
+test.to_csv("src/Jack/Data/brown_test.csv", sep = "\t", index = False)
+cal.to_csv("src/Jack/Data/brown_cal.csv", sep = "\t", index = False)
--- src/Jack/BERT Cloze/word_masking.py
+++ src/Jack/BERT Cloze/word_masking.py
@@ -1,34 +1,76 @@
 from itertools import count
 from numpy import core, result_type
+from torch._C import device
 from transformers import BertTokenizer, BertForMaskedLM
 import torch
 import pandas as p
 import random
 from pandas.core.series import Series 
+from transformers import TrainingArguments
+from transformers import Trainer
+from sklearn.model_selection import train_test_split as tt_split
 
-#load cleaned data from file:
-def load_data():
-    with open("src/Jack/Data/brown_string.txt", "r") as file:
-        corpus = file.read()
-    return corpus
 
-corpus = load_data()
+
+random.seed(24)
+
+master = p.read_csv("src/Jack/Data/brown_master.csv", sep = "\t")
+
+
 
 #Bert pieces initializing 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-inputs = tokenizer(corpus, return_tensors = "pt")
-inputs.keys()
+#Tokenize words
+#For now, using bert base input size of 512
+#warning: Pretty slow, only run when necessary
+inputs = tokenizer(clean_sentences, return_tensors = "pt", max_length=512, truncation=True, padding="max_length")
 
+#clone input_ids to get labels tensor
 inputs["labels"] = inputs.input_ids.detach().clone()
 
+#get indexes of masked word in each sentence
+masked_inds = get_masked_inds(sentences)
+len(masked_inds)
+
+#replace words at masked_ind with the mask token, which has ID 103
+for i in range(inputs.input_ids.shape[0]):
+    inputs.input_ids[i, masked_inds[i]] = 103
 
 
-
-
-
+class BrownDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings):
+        self.encodings = encodings
     
+    def __getitem__(self, idx):
+        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+    
+    def __len__(self):
+        return len(self.encodings.input_ids)
+
+dataset = BrownDataset(inputs)
+loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
+
+
+
+args = TrainingArguments(
+    output_dir="out",
+    per_device_train_batch_size=16,
+    num_train_epochs=2
+)
+
+
+trainer = Trainer(
+    model=model,
+    args=args,
+    train_dataset=dataset
+)
+
+trainer.train()
+
+
+
 
 
 
